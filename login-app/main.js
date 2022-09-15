8922208f04f935683067b5f334278d8446469d51
ajay0325 <m.ajay030398@gmail.com>
2022-09-15 20:43:41 +0530

    interview

---
+++ login-app/main.js
@@ -0,0 +1,27 @@
+// var num = prompt("Enter the number")
+// var sum = 0;
+// var rem = 0;
+
+// while (num) {
+//     rem = num%10;
+//     sum = sum+rem;
+//     num = (num-rem)/10
+// }
+// console.log(sum);
+
+// var b = [1,2,3,10,11,35,24,89,12,65,45];
+// const ascendorder = b.sort(function(a,b){
+//     return a-b;
+// })
+// console.log(ascendorder);
+
+
+function Email(email) {
+    
+    if (email=='aathi03@yahoo.com') {
+        console.log('valid')
+    } else {
+        console.log('invalid');
+    }
+}
+Email('aathi03yahoo.com');
